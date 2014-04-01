a51e9e99f1576c3c518b549a671677251cca29d6
Daniel Barowy <dbarowy@cs.umass.edu>
2014-03-31 21:38:13 -0400

    initial

---
+++ Spreadsheet/ExcelSpreadsheet.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace NoSheet
+{
+    public class ExcelSpreadsheet : ISpreadsheet, IDisposable
+    {
+        private Excel.Application _app;
+        private Excel.Workbook _wb;
+        private HashSet<Excel.Worksheet> _wss = new HashSet<Excel.Worksheet>();
+
+        // All of the following private enums are poorly documented
+        private enum XlCorruptLoad
+        {
+            NormalLoad = 0,
+            RepairFile = 1,
+            ExtractData = 2
+        }
+
+        private enum XlUpdateLinks
+        {
+            Yes = 2,
+            No = 0
+        }
+
+        private enum XlPlatform
+        {
+            Macintosh = 1,
+            Windows = 2,
+            MSDOS = 3
+        }
+
+        public ExcelSpreadsheet(string filename)
+        {
+            _app = ExcelSingleton.Instance;
+            _wb = OpenWorkbook(filename);
+        }
+
+        private void FastRead()
+        {
+
+        }
+
+        private Excel.Workbook OpenWorkbook(string filename)
+        {
+            // we need to disable all alerts, e.g., password prompts, etc.
+            _app.DisplayAlerts = false;
+
+            // disable macros
+            _app.AutomationSecurity = Microsoft.Office.Core.MsoAutomationSecurity.msoAutomationSecurityForceDisable;
+
+            // This call is stupid. See:
+            // http://msdn.microsoft.com/en-us/library/microsoft.office.interop.excel.workbooks.open%28v=office.11%29.aspx
+            _app.Workbooks.Open(filename, // FileName (String)
+                                XlUpdateLinks.Yes, // UpdateLinks (XlUpdateLinks enum)
+                                true, // ReadOnly (Boolean)
+                                Missing.Value, // Format (int?)
+                                "thisisnotapassword", // Password (String)
+                                Missing.Value, // WriteResPassword (String)
+                                true, // IgnoreReadOnlyRecommended (Boolean)
+                                Missing.Value, // Origin (XlPlatform enum)
+                                Missing.Value, // Delimiter; if the filetype is txt (String)
+                                Missing.Value, // Editable; not what you think (Boolean)
+                                false, // Notify (Boolean)
+                                Missing.Value, // Converter(int)
+                                false, // AddToMru (Boolean)
+                                Missing.Value, // Local; really "use my locale?" (Boolean)
+                                XlCorruptLoad.RepairFile); // CorruptLoad (XlCorruptLoad enum)
+
+            return _app.Workbooks[1];
+        }
+
+        public void Dispose()
+        {
+            // close workbook
+            _wb.Close();
+
+            // release COM object
+            Marshal.ReleaseComObject(_wb);
+
+            // nullify ref
+            _wb = null;
+
+            // poke GC
+            GC.Collect();
+        }
+    }
+
+    public class ExcelSingleton
+    {
+        private static Excel.Application _instance;
+        private ExcelSingleton() { }
+        public static Excel.Application Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Excel.Application();
+                }
+                return _instance;
+            }
+        }
+
+        private ~ExcelSingleton()
+        {
+            _instance.Quit();
+            _instance = null;
+            GC.Collect();
+        }
+    }
+}
---
+++ Spreadsheet/ISpreadsheet.cs
@@ -0,0 +1,10 @@
+﻿namespace NoSheet
+{
+    public interface ISpreadsheet
+    {
+        public void InsertValue(AST.Address address, string value);
+        public string GetValue(AST.Address address);
+        public string GetFormula(AST.Address address);
+        public bool IsFormula(AST.Address address);
+    }    
+}
