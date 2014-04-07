381c2db6223d0388b500a836d55e6a6dfe286ccf
Daniel Barowy <dbarowy@cs.umass.edu>
2014-04-07 09:31:01 -0400

    Added missing source.

---
+++ SpreadsheetAST/Graph.fs
@@ -0,0 +1,54 @@
+﻿module Graph
+    open System.Collections.Generic
+    open SpreadsheetAST
+    type DirectedAcyclicGraph(formulas: Dictionary<Address,Expression>, data: Dictionary<Address,string>) =
+        // these are all of the input addresses for a formula output
+        // note that some inputs are data and others are formulas
+        let formula_inputs =
+            Seq.map (fun (pair: KeyValuePair<Address,Expression>) ->
+                let addr = pair.Key
+                let expr = pair.Value
+                let ranges = SpreadsheetUtility.GetRangesFromExpr(expr)
+                let addrs = SpreadsheetUtility.GetAddressesFromExpr(expr)
+                let raddrs = Set.unionMany (Set.map (fun (r: Range) -> r.GetAddresses()) ranges)
+                (addr, (Set.union addrs raddrs))
+            ) formulas |> Map.ofSeq
+
+        // these are all of the outputs that depend on a particular input
+        let cell_outputs =
+            Seq.map (fun (pair: KeyValuePair<Address,string>) ->
+                let iaddr = pair.Key
+                let outputs = Seq.map (fun (pair: KeyValuePair<Address,Expression>) ->
+                                let faddr = pair.Key
+                                if formula_inputs.[faddr].Contains(iaddr) then
+                                    Some(faddr)
+                                else
+                                    None
+                              ) formulas |> Seq.choose id |> Set.ofSeq
+                (iaddr, outputs)
+            ) data |> Map.ofSeq
+
+        // this returns addresses of all cells that provide input *data* for a formula
+        // note that this computes the transitive closure of the "is input to" relation
+        member self.GetInputDependencies(formula_address: Address) : Set<Address> =
+            let rec GetInputs(f: Address) : Set<Address> =
+                // if f is a formula then get the addresses
+                // of its inputs
+                if formula_inputs.ContainsKey(f) then
+                    Set.map (fun input ->
+                        GetInputs(input)
+                    ) (formula_inputs.[f]) |> Set.unionMany
+                // if f is not a formula then it IS an input
+                else
+                    set [f]
+            GetInputs(formula_address)
+
+        // this returns all output formulas that depend on a particular input
+        // note that this computes the transitive closure of the "is output for" relation
+        member self.GetOutputDependencies(input_address: Address) : Set<Address> =
+            cell_outputs.[input_address]
+
+        // this returns only the ranges that are referenced directly in this formula
+        // note: used for classic DataDebug algorithm
+        member self.GetInputRanges(expr: Expression) : Set<Range> =
+            SpreadsheetUtility.GetRangesFromExpr(expr)
---
+++ SpreadsheetAST/Utility.fs
@@ -0,0 +1,74 @@
+﻿module SpreadsheetUtility
+    open SpreadsheetAST
+    open System.Collections.Generic
+
+    let PuntedFunction(fnname: string) : bool =
+        match fnname with
+        | "INDEX" -> true
+        | "HLOOKUP" -> true
+        | "VLOOKUP" -> true
+        | "LOOKUP" -> true
+        | "OFFSET" -> true
+        | _ -> false
+
+    let rec GetRangesFromRangeReference(ref: ReferenceRange) : Set<Range> = set [ref.Range]
+
+    and GetRangesFromFunction(ref: ReferenceFunction) : Set<Range> =
+        if PuntedFunction(ref.FunctionName) then
+            Set.empty
+        else
+            List.map (fun arg -> GetRangesFromExpr(arg)) ref.ArgumentList |> Set.unionMany
+        
+    and GetRangesFromExpr(expr: Expression) : Set<Range> =
+        match expr with
+        | ReferenceExpr(r) -> GetRangesFromRef(r)
+        | BinOpExpr(op, e1, e2) -> Set.union (GetRangesFromExpr(e1)) (GetRangesFromExpr(e2))
+        | UnaryOpExpr(op, e) -> GetRangesFromExpr(e)
+        | ParensExpr(e) -> GetRangesFromExpr(e)
+
+    and GetRangesFromRef(ref: Reference) : Set<Range> =
+        match ref with
+        | :? ReferenceRange as r -> GetRangesFromRangeReference(r)
+        | :? ReferenceAddress -> Set.empty
+        | :? ReferenceNamed -> Set.empty   // TODO: symbol table lookup
+        | :? ReferenceFunction as r -> GetRangesFromFunction(r)
+        | :? ReferenceConstant -> Set.empty
+        | :? ReferenceString -> Set.empty
+        | _ -> failwith "Unknown reference type."
+
+    let rec GetAddressesFromExpr(expr: Expression) : Set<Address> =
+        match expr with
+        | ReferenceExpr(r) -> GetAddressesFromRef(r)
+        | BinOpExpr(op, e1, e2) -> Set.union (GetAddressesFromExpr(e1)) (GetAddressesFromExpr(e2))
+        | UnaryOpExpr(op, e) -> GetAddressesFromExpr(e)
+        | ParensExpr(e) -> GetAddressesFromExpr(e)
+
+    and GetAddressesFromRef(ref: Reference) : Set<Address> =
+        match ref with
+        | :? ReferenceRange -> Set.empty
+        | :? ReferenceAddress as r -> GetAddressesFromAddressRef(r)
+        | :? ReferenceNamed -> Set.empty   // TODO: symbol table lookup
+        | :? ReferenceFunction as r -> GetAddressesFromFunction(r)
+        | :? ReferenceConstant -> Set.empty
+        | :? ReferenceString -> Set.empty
+        | _ -> failwith "Unknown reference type."
+
+    and GetAddressesFromAddressRef(ref: ReferenceAddress) : Set<Address> = set [ref.Address]
+
+    and GetAddressesFromFunction(ref: ReferenceFunction) : Set<Address> =
+        if PuntedFunction(ref.FunctionName) then
+            Set.empty
+        else
+            List.map (fun arg -> GetAddressesFromExpr(arg)) ref.ArgumentList |> Set.unionMany
+
+    let rec GetFormulaNamesFromExpr(ast: Expression): Set<string> =
+        match ast with
+        | ReferenceExpr(r) -> GetFormulaNamesFromReference(r)
+        | BinOpExpr(op, e1, e2) -> Set.union (GetFormulaNamesFromExpr(e1)) (GetFormulaNamesFromExpr(e2))
+        | UnaryOpExpr(op, e) -> GetFormulaNamesFromExpr(e)
+        | ParensExpr(e) -> GetFormulaNamesFromExpr(e)
+
+    and GetFormulaNamesFromReference(ref: Reference): Set<string> =
+        match ref with
+        | :? ReferenceFunction as r -> set [r.FunctionName]
+        | _ -> Set.empty
