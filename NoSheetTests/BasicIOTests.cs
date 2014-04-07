975fb47fbe1a9c73b9fa1a7ec240b4204ec3d6bf
Daniel Barowy <dbarowy@cs.umass.edu>
2014-04-07 11:50:42 -0400

    Add tests.

---
+++ NoSheetTests/BasicIOTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Reflection;
+using NoSheet;
+
+namespace NoSheetTests
+{
+    [TestClass]
+    public class BasicIOTests
+    {
+        public const string SIMPLE_WB = "SimpleWorkbook.xlsx";
+
+        [TestMethod]
+        public void OpenReadClose()
+        {
+            // get test file path
+            string filename = ResourceLoader.GetResourcePath(SIMPLE_WB);
+            
+            // load spreadsheet
+            using (var ss = new ExcelSpreadsheet(filename))
+            {
+                var values = ss.GetAllValues();
+                var formulas = ss.GetAllFormulas();
+
+                // there should be 23 data cells
+                Assert.AreEqual(23, values.Count);
+
+                // there should be 3 formulas
+                Assert.AreEqual(3, formulas.Count);
+            }
+        }
+    }
+}
---
+++ NoSheetTests/ResourceLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NoSheetTests
+{
+    public static class ResourceLoader
+    {
+        /// <summary>
+        /// This method returns the correct resource path regardless of
+        /// output directory, as long as resources are marked "Copy to
+        /// Output Directory."
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static string GetResourcePath(string filename)
+        {
+            string OUTPUT_PATH = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string DATA_DIRECTORY_NAME = "TestData";
+            string path = Path.Combine(OUTPUT_PATH, DATA_DIRECTORY_NAME, filename);
+            Assert.IsTrue(File.Exists(path), String.Format("Cannot find test data file \"{0}\" at path \"{1}\".", filename, path));
+            return path;
+        }
+    }
+}
