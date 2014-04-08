c2129c72614aa9161004962b81a071a1934b6ac1
Daniel Barowy <dbarowy@cs.umass.edu>
2014-04-08 17:28:50 -0400

    Added range write test and stub for interleaved range write test.

--- NoSheetTests/BasicIOTests.cs
+++ NoSheetTests/BasicIOTests.cs
@@ -77,7 +77,8 @@ namespace NoSheetTests
 
             // make a copy of the file to the following path
             string newfilename = Path.Combine(Path.GetDirectoryName(filename),
-                                              Path.GetFileNameWithoutExtension(filename) + "_flushtest.xlsx");
+                                              Path.GetFileNameWithoutExtension(filename) +
+                                              "_flushtest.xlsx");
 
             // ensure that the file doesn't already exist
             Assert.IsFalse(File.Exists(newfilename));
@@ -153,6 +154,93 @@ namespace NoSheetTests
             // if we got here, then our ExcelSpreadsheet constructor
             // failed to throw the appropriate exception
             Assert.Fail("ExcelSpreadsheet should throw a FileNotFoundException when a file does not exist.");
+        }
+
+        [TestMethod]
+        public void OpenUpdateRangeFlushClose()
+        {
+            // get test file path
+            string filename = ResourceLoader.GetResourcePath(SIMPLE_WB);
+
+            // make a copy of the file to the following path
+            string newfilename = Path.Combine(Path.GetDirectoryName(filename),
+                                              Path.GetFileNameWithoutExtension(filename) +
+                                              "_flushrangetest.xlsx");
+
+            // ensure that the file doesn't already exist
+            Assert.IsFalse(File.Exists(newfilename));
+
+            // load spreadsheet
+            using (var ss = new ExcelSpreadsheet(filename))
+            {
+                // save with new name
+                ss.SaveAs(newfilename);
+
+                // some addresses
+                var addr_a1 = Address.FromR1C1(1, 1, "Sheet1", ss.WorkbookName, ss.Directory);
+                var addr_a2 = Address.FromR1C1(2, 1, "Sheet1", ss.WorkbookName, ss.Directory);
+                var addr_a3 = Address.FromR1C1(3, 1, "Sheet1", ss.WorkbookName, ss.Directory);
+                var addr_a5 = Address.FromR1C1(5, 1, "Sheet1", ss.WorkbookName, ss.Directory);
+                var addr_b1 = Address.FromR1C1(1, 2, "Sheet1", ss.WorkbookName, ss.Directory);
+                var addr_c1 = Address.FromR1C1(1, 3, "Sheet1", ss.WorkbookName, ss.Directory);
+
+                // get the value in Sheet1!A1
+                var a1_orig = ss.ValueAt(addr_a1);
+                var a2_orig = ss.ValueAt(addr_a2);
+                var a3_orig = ss.ValueAt(addr_a3);
+                var a5_orig = ss.ValueAt(addr_a5);
+
+                // get the values of the function outputs at Sheet1!B1 and Sheet1!C1
+                var b1_orig = ss.ValueAt(addr_b1);
+                var c1_orig = ss.ValueAt(addr_c1);
+
+                // the new values
+                var a1_newval = System.Convert.ToString(System.Convert.ToDouble(a1_orig) + 1);
+                var a2_newval = System.Convert.ToString(System.Convert.ToDouble(a2_orig) + 1);
+                var a3_newval = System.Convert.ToString(System.Convert.ToDouble(a3_orig) + 1);
+                var a5_newval = System.Convert.ToString(System.Convert.ToDouble(a5_orig) + 1);
+
+                // change the values
+                ss.SetValueAt(addr_a1, a1_newval);
+                ss.SetValueAt(addr_a2, a2_newval);
+                ss.SetValueAt(addr_a3, a3_newval);
+                ss.SetValueAt(addr_a5, a5_newval);
+
+                // get the values again
+                var a1_new = ss.ValueAt(addr_a1);
+                var a2_new = ss.ValueAt(addr_a2);
+                var a3_new = ss.ValueAt(addr_a3);
+                var a5_new = ss.ValueAt(addr_a5);
+
+                // get the new function outputs
+                var b1_new = ss.ValueAt(addr_b1);
+                var c1_new = ss.ValueAt(addr_c1);
+
+                // the values should be different
+                Assert.AreNotEqual(a1_orig, a1_new);
+                Assert.AreNotEqual(a2_orig, a2_new);
+                Assert.AreNotEqual(a3_orig, a3_new);
+                Assert.AreNotEqual(a5_orig, a5_new);
+
+                // specifically, the new values should be the values we stuck in there
+                Assert.AreEqual(a1_new, a1_newval);
+                Assert.AreEqual(a2_new, a2_newval);
+                Assert.AreEqual(a3_new, a3_newval);
+                Assert.AreEqual(a5_new, a5_newval);
+
+                // the formula outputs should also be different
+                Assert.AreNotEqual(b1_orig, b1_new);
+                Assert.AreNotEqual(c1_orig, c1_new);
+            }
+
+            // cleanup
+            File.Delete(newfilename);
+        }
+
+        [TestMethod]
+        public void InterleavedFunctionValueWrite()
+        {
+            // TODO
         }
     }
 }
