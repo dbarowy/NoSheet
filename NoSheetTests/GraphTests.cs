3ae9bd55a73ae78b96e28d7591cf493d8f43bc2c
Daniel Barowy <dbarowy@cs.umass.edu>
2014-04-15 11:48:32 -0400

    Added test suite for graph functions.

---
+++ NoSheetTests/GraphTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NoSheet;
+using SpreadsheetAST;
+
+namespace NoSheetTests
+{
+    [TestClass]
+    public class GraphTests
+    {
+        public const string RVARIATIONS = "RangeVariations.xlsx";
+
+        [TestMethod]
+        public void ExcludesNonPerturbableRange()
+        {
+            // get test file path
+            string filename = ResourceLoader.GetResourcePath(RVARIATIONS);
+
+            using (var ss = new ExcelSpreadsheet(filename))
+            {
+                var rs = ss.HomogeneousInputs;
+
+                foreach (SpreadsheetAST.Range r in rs)
+                {
+                    var rstr = r.A1FullyQualified();
+                }
+
+                Assert.IsTrue(rs.Length > 0);
+            }
+        }
+    }
+}
